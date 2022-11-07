43078ffba5b9a0e24ded9577401d0cab5f4c659b
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-07 11:40:26 -0300

    Requisito 2, sem comentario

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // return data.species.animal.every((animals) => animals.age >= age);
   const objAnimais = data.species.find((item) => item.name === animal);
   return objAnimais.residents.every((listAnimal) => listAnimal.age >= age);
 }
