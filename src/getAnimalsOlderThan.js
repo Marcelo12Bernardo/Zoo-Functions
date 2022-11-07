60dd3026b52ddf7e0ca3fa5f61783c8e1e6c00c9
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-07 11:39:48 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // return data.species.animal.every((animals) => animals.age >= age);
+  const objAnimais = data.species.find((item) => item.name === animal);
+  return objAnimais.residents.every((listAnimal) => listAnimal.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  if (ids !== undefined) {
+    const epc = data.species.filter((item) => item.id === ids);
+    return epc;
+  }
+  return [];
 }
 
 module.exports = getSpeciesByIds;
