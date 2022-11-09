7490e387bf1272719743bc8f1ec56b121763426b
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-09 11:47:06 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Passando count como parametron deve retornar 4', () => expect(handlerElephants('count')).toEqual(4));
+  it('Passando names como parametron deve retornar Jefferson', () => expect(handlerElephants('names').includes('Jefferson')).toBeTruthy());
+  it('Passando averageAge como parametron deve retornar 10.5', () => expect(handlerElephants('averageAge')).toEqual(10.5));
+});
