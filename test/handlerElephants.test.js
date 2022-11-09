b7da04a7f80620888c6805790935fb1c502614be
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-09 12:16:14 -0300

    Requisito  8

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,7 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('Passando count como parametron deve retornar 4', () => expect(handlerElephants('count')).toEqual(4));
-  it('Passando names como parametron deve retornar Jefferson', () => expect(handlerElephants('names').includes('Jefferson')).toBeTruthy());
-  it('Passando averageAge como parametron deve retornar 10.5', () => expect(handlerElephants('averageAge')).toEqual(10.5));
+  it('Passando count deve retornar 4', () => expect(handlerElephants('count')).toEqual(4));
+  it('Passando names deve retornar Jefferson', () => expect(handlerElephants('names').includes('Jefferson')).toBeTruthy());
+  it('Passando averageAge deve retornar 10.5', () => expect(handlerElephants('averageAge')).toEqual(10.5));
+  it('Passando popularity deve retornar um número igual ou maior a 5', () => expect(handlerElephants('popularity') >= 5).toBeTruthy());
+  it('Passando availability deve retornar um array', () => expect(typeof handlerElephants('availability') === 'object').toBeTruthy());
+  it('Availability não deve conter Monday', () => expect(handlerElephants('availability').includes('Monday')).toBeFalsy());
+  it('Passando location deve retornar undefined', () => expect(handlerElephants()).toEqual(undefined));
+  it('Passando location deve retornar uma string', () => expect(typeof handlerElephants('location') === 'string').toBeTruthy());
+  it('Passando location deve retornar nw', () => expect(handlerElephants('location')).toEqual('NW'));
 });
