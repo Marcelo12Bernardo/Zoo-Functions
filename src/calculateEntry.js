cd0fc2da41433c46558d0d445d2823ae3e1f8cb8
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-09 20:07:53 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants !== {}) {
+    const totalPorFaixa = {
+      child: entrants.filter((person) => person.age < 18).length,
+      adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+      senior: entrants.filter((person) => person.age >= 50).length,
+    };
+    return totalPorFaixa;
+  }
+  return 0;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
-  //  Inicio
+  if (!entrants || JSON.stringify(entrants) === '{}') { return 0; }
+  const totPessoas = countEntrants(entrants);
+  const ingressoChild = data.prices.child;
+  const ingressoAdult = data.prices.adult;
+  const ingressoSenior = data.prices.senior;
+  const valorTotal = (ingressoChild * totPessoas.child)
+  + (ingressoAdult * totPessoas.adult)
+  + (ingressoSenior * totPessoas.senior);
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
