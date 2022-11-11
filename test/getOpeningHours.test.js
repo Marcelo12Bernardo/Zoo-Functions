d3d67f28727cac19cfa4a8a43360efc934160848
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-11 12:50:50 -0300

    Lint + correção

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -20,10 +20,9 @@ describe('Testes da função getOpeningHours', () => {
   const minuteError = 'The minutes must be between 0 and 59';
   const dayError = 'The day must be valid. Example: Monday';
   // testa se retorna os  erros corretos
-  test('Verifica se ao passar \'qualquer coisa\' retorna um erro', () => expect(() => getOpeningHours('Ramdom')).toThrow(dayError));
+  it('Verifica se ao passar \'qualquer coisa\' retorna um erro', () => expect(() => getOpeningHours('Ramdom')).toThrow(dayError));
   test('Verifica se ao passar a \'hora errada\' retorna um erro', () => expect(() => getOpeningHours('Monday', 'Ramdom')).toThrow(hourError));
-  test('Verifica se ao passar o dia errado retorna um erro', () => expect(() => getOpeningHours('Ramdom', '08:00-AM')).toThrow(dayError));
-  test('Verifica se ao passar uma \'hora maior que 12\' retorna um erro', () => expect(() => getOpeningHours('Monday', '17:00-AM')).toThrow(hourError2));
-  test('Verifica se ao passar um \'minuto maior que 59\' retorna um erro', () => expect(() => getOpeningHours('Monday', '10:95-AM')).toThrow(minuteError));
-
+  it('Verifica se ao passar o dia errado retorna um erro', () => expect(() => getOpeningHours('Ramdom', '08:00-AM')).toThrow(dayError));
+  it('Verifica se ao passar uma \'hora maior que 12\' retorna um erro', () => expect(() => getOpeningHours('Monday', '17:00-AM')).toThrow(hourError2));
+  it('Verifica se ao passar um \'minuto maior que 59\' retorna um erro', () => expect(() => getOpeningHours('Monday', '10:95-AM')).toThrow(minuteError));
 });
