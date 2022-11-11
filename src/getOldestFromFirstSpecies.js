d799da940bb9cd4160c2c7823341d50f8c751451
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-11 11:55:09 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pessoaColaboradora = data.employees.find((cuidadores) => cuidadores.id === id);
+  const primeiroResponsavel = pessoaColaboradora.responsibleFor[0];
+  const especie = data.species.filter((animais) => animais.id === primeiroResponsavel);
+  const maiorIdade = especie[0].residents.reduce((acc, curr) => Math.max(acc, curr.age), 0);
+  const maisVelho = especie[0].residents.find((animal) => animal.age === maiorIdade);
+  return Object.values(maisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
