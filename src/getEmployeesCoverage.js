74342eaedc1386ff3f8fad4a9c32aceec99fa68b
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-14 20:41:48 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees } = data;
+const { species } = data;
+
+const findEmploy = employees.map((cuidador) => {
+  const { id, firstName, lastName } = cuidador;
+  const responsavelP = cuidador.responsibleFor.map((idAnimals) => species
+    .find((specie) => specie.id === idAnimals).name);
+  const locais = cuidador.responsibleFor.map((idAnimals) => species
+    .find((specie) => specie.id === idAnimals).location);
+  const dadosEmploy = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: responsavelP,
+    locations: locais,
+  };
+  return dadosEmploy;
+});
+function getEmployeesCoverage(objEmploy) {
+  let busca;
+  if (!objEmploy) {
+    return findEmploy;
+  }
+  if (objEmploy.name) {
+    busca = findEmploy.find((name) => name.fullName.includes(objEmploy.name));
+  }
+  if (objEmploy.id) {
+    busca = findEmploy.find((name) => name.id === objEmploy.id);
+  }
+  if (busca === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return busca;
 }
 
 module.exports = getEmployeesCoverage;
